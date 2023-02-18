3c5a0a5526c2dac2d7c16c76de05982bdff83b29
Nicholas Brink <nicbrink@gmail.com>
2023-02-18 13:52:08 +0000

    added table of contents with links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,17 +2,25 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ${data.tableOfContents}
+  ## Table of Contents
+  1. [Description](#description)   //[text with the link](URL of the link) 
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Licence](#licence)
+  5. [Contributing](#contributing)
+  6. [Tests](#Tests)
+  7. [Questions](#github)
+
   ## Description
   ${data.description}
-
-  ## Table of Contents
-  ${data.tableOfContents}
 
   ## Installation
   ${data.installation}
 
   ## Usage
-  ${data.description}
+  ${data.usage}
 
   ## License
   ${data.license}
